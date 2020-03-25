c4de880fe03de2d70269b725094a127d9b3e3bbf
ac2pic <ac2pic@gmail.com>
2020-03-25 12:56:03 -0500

    Added basic template for DynamicJson

---
+++ plugin.js
@@ -0,0 +1,18 @@
+export default class DynamicJson extends Plugin {
+	constructor(mod) {
+		super();
+		this.mod = mod;
+	}
+
+	async preload() {
+
+	}
+
+	async postload() {
+
+	}
+
+	async prestart() {
+
+	}
+}
