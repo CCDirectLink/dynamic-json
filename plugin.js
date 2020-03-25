038be70b5eb9814880ca6915761c8a499d19d6a1
ac2pic <ac2pic@gmail.com>
2020-03-25 16:17:24 -0500

    Removed more unnecessary stuff

--- plugin.js
+++ plugin.js
@@ -2,10 +2,18 @@ export default class DynamicJson extends Plugin {
 	constructor(mod) {
 		super();
 		this.mod = mod;
+		this.intercept = {};
 	}
 
 	async preload() {
-
+		window.registerIntercept = function(url, cb) {
+			this.intercept[url] = cb;
+		}
+		this.intercept["a.json"] = function() {
+			return {
+				a: 2
+			};
+		}
 	}
 
 	async postload() {
