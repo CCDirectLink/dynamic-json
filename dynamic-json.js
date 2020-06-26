f5427c580ecaf28dfca1eb2306eafedbc4d9b1ce
ac2pic <ac2pic@gmail.com>
2020-06-26 17:12:40 -0500

    Updated to work with ccloader3

---
+++ dynamic-json.js
@@ -0,0 +1,20 @@
+
+
+export default class DynamicJson {
+	constructor(mod) {
+		debugger
+		this.mod = mod;
+	}
+
+	async preload() {
+		console.log('I work!');
+	}
+
+	async postload() {
+
+	}
+
+	async prestart() {
+
+	}
+}
