2f6866bdd9a5edf6d01d8ba6a44ba03f62061c34
ac2pic <ac2pic@gmail.com>
2021-09-23 21:00:54 -0500

    Added support for CCLoader

--- main.js
+++ main.js
@@ -1,5 +1,50 @@
 import DynamicJson from './dynamic-json.js';
 const djson = new DynamicJson;
+
+
+
+function loadStage(url, isModule) {
+	return new Promise((resolve, reject) => {
+		const script = document.createElement('script');
+		script.onload = () => resolve();
+		script.onerror = () => reject();
+		script.type = isModule ? 'module' : 'text/javascript';
+		script.src = url;
+		document.head.appendChild(script);
+	});
+}
+
+async function getMods() {
+	if (typeof modloader === 'object' && typeof modloader.loadedMods === 'object') {
+		return modloader.loadedMods;
+	}
+
+	const mods = new Map;
+	// This is ccloader
+	if (window.activeMods) {
+		for (const activeMod of window.activeMods) {
+			// if they do not have a ccmod.json then skip them
+			let manifest;
+			try {
+				manifest = await fetch('/' + activeMod.baseDirectory + 'ccmod.json').then(e => e.json());
+			} catch (e) {
+				continue;
+			}
+			activeMod.executeStage = async (name) => {
+				const stageValue = manifest[name];
+				if (typeof stageValue  === 'string') {
+					await loadStage('/' + activeMod.baseDirectory + stageValue, mod.module);
+				}
+			};
+
+			mods.set(activeMod.name, activeMod);
+		}
+	}
+
+	return mods;
+}
+
+
 export default class Main {
 	constructor(mod) {
 		this.mod = mod;
@@ -7,13 +52,13 @@ export default class Main {
 
 	async preload() {
 		window.DynamicJson = djson;
-        for (const [name, mod] of modloader.loadedMods) {
-            try {
-                await mod.executeStage('registerDynamicJsonGenerators');
-            } catch (e) {
-                console.log('Mod', e, 'failed');
-                console.log(e.toString());
-            }
+		const mods = await getMods();
+		for (const [name, mod] of mods) {
+			try {
+				await mod.executeStage('registerDynamicJsonGenerators');
+			} catch (e) {
+				console.log(`[${name}] An error occured:\n${e.toString()}`);
+			}			
 		}
 		window.DynamicJson = undefined;
 	}
